8f9058eb508466f747e248df0f7d686eb48fdac0
zhourunlong <zhourunlongvector@gmail.com>
2020-09-21 20:34:40 +0800

    finished PA0

---
+++ PA0-IIIS2020/code/build/CMakeFiles/PA0.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/src/canvas_parser.cpp" "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/build/CMakeFiles/PA0.dir/src/canvas_parser.cpp.o"
+  "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/src/image.cpp" "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/build/CMakeFiles/PA0.dir/src/image.cpp.o"
+  "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/src/main.cpp" "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/build/CMakeFiles/PA0.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../deps/vecmath/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vectorzhou/ComputerGraphics/PA0-IIIS2020/code/build/deps/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- PA0-IIIS2020/code/include/element.hpp
+++ PA0-IIIS2020/code/include/element.hpp
@@ -5,6 +5,8 @@
 #include <queue>
 #include <cstdio>
 
+const int Dx[4] = {-1, 0, 1, 0}, Dy[4] = {0, 1, 0, -1};
+
 class Element {
 public:
     virtual void draw(Image &img) = 0;
@@ -18,7 +20,32 @@ public:
     int xB, yB;
     Vector3f color;
     void draw(Image &img) override {
-        // TODO: Implement Bresenham Algorithm
+        int dx = xB - xA, dy = yB - yA, e, x, y, sgn;
+        if (abs(dx) >= abs(dy)) {
+            if (xA > xB) {
+                std::swap(xA, xB); dx = -dx;
+                std::swap(yA, yB); dy = -dy;
+            }
+            e = -dx; x = xA; y = yA;
+            sgn = dy >= 0 ? 1 : -1; dy = std::abs(dy);
+            for (int i = 0; i <= dx; ++i) {
+                img.SetPixel(x, y, color);
+                ++x; e += 2 * dy;
+                if (e >= 0) {y += sgn; e -= 2 * dx;}
+            }
+        } else {
+            if (yA > yB) {
+                std::swap(xA, xB); dx = -dx;
+                std::swap(yA, yB); dy = -dy;
+            }
+            e = -dy; x = xA; y = yA;
+            sgn = dx >= 0 ? 1 : -1; dx = std::abs(dx);
+            for (int i = 0; i <= dy; ++i) {
+                img.SetPixel(x, y, color);
+                ++y; e += 2 * dx;
+                if (e >= 0) {x += sgn; e -= 2 * dy;}
+            }
+        }
         printf("Draw a line from (%d, %d) to (%d, %d) using color (%f, %f, %f)\n", xA, yA, xB, yB,
                 color.x(), color.y(), color.z());
     }
@@ -31,7 +58,23 @@ public:
     int radius;
     Vector3f color;
     void draw(Image &img) override {
-        // TODO: Implement Algorithm to draw a Circle
+        int x, y = radius, e = 5 - 4 * radius;
+        while (x <= y) {
+            img.SetPixel(cx + x, cy + y, color);
+            img.SetPixel(cx + y, cy + x, color);
+            img.SetPixel(cx - x, cy + y, color);
+            img.SetPixel(cx - y, cy + x, color);
+            img.SetPixel(cx + x, cy - y, color);
+            img.SetPixel(cx + y, cy - x, color);
+            img.SetPixel(cx - x, cy - y, color);
+            img.SetPixel(cx - y, cy - x, color);
+            if (e < 0) e += 8 * x + 12;
+            else {
+                e += 8 * (x - y) + 20;
+                --y;
+            }
+            ++x;
+        }
         printf("Draw a circle with center (%d, %d) and radius %d using color (%f, %f, %f)\n", cx, cy, radius,
                color.x(), color.y(), color.z());
     }
@@ -42,8 +85,24 @@ class Fill : public Element {
 public:
     int cx, cy;
     Vector3f color;
+    std::queue <std::pair <int, int> > Q;
     void draw(Image &img) override {
-        // TODO: Flood fill
+        int w = img.Width(), h = img.Height();
+        Vector3f oldcolor = img.GetPixel(cx, cy);
+        std::pair <int, int> u = std::make_pair(cx, cy);
+        Q.push(u);
+        while (!Q.empty()) {
+            u = Q.front(); Q.pop();
+            int x = u.first, y = u.second;
+            if (img.GetPixel(x, y) == oldcolor) {
+                img.SetPixel(x, y, color);
+                for (int k = 0; k < 4; ++k) {
+                    int tx = x + Dx[k], ty = y + Dy[k];
+                    if (tx < 0 || tx >= w || ty < 0 || ty >= h) continue;
+                    Q.push(std::make_pair(tx, ty));
+                }
+            }
+        }
         printf("Flood fill source point = (%d, %d) using color (%f, %f, %f)\n", cx, cy,
                 color.x(), color.y(), color.z());
     }
