64b4cf8d1ce264a381e6ffd2628fcf982460620a
zhourunlong <zhourunlongvector@gmail.com>
2020-12-03 17:20:11 +0800

    init RayTracing project

---
+++ RT/code/include/mesh.hpp
@@ -0,0 +1,131 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <utility>
+#include <sstream>
+#include "object3d.hpp"
+#include "triangle.hpp"
+#include "Vector2f.h"
+#include "Vector3f.h"
+
+class Mesh : public Object3D {
+
+public:
+    Mesh(const char *filename, Material *material) : Object3D(material) {
+        triangles.clear();
+        //deleteTree(rt);
+        // Optional: Use tiny obj loader to replace this simple one.
+        std::ifstream f;
+        f.open(filename);
+        if (!f.is_open()) {
+            std::cout << "Cannot open " << filename << "\n";
+            return;
+        }
+        std::string line;
+        std::string vTok("v");
+        std::string fTok("f");
+        std::string texTok("vt");
+        char bslash = '/', space = ' ';
+        std::string tok;
+        int texID;
+        while (true) {
+            std::getline(f, line);
+            if (f.eof()) {
+                break;
+            }
+            if (line.size() < 3) {
+                continue;
+            }
+            if (line.at(0) == '#') {
+                continue;
+            }
+            std::stringstream ss(line);
+            ss >> tok;
+            if (tok == vTok) {
+                Vector3f vec;
+                ss >> vec[0] >> vec[1] >> vec[2];
+                v.push_back(vec);
+            } else if (tok == fTok) {
+                if (line.find(bslash) != std::string::npos) {
+                    std::replace(line.begin(), line.end(), bslash, space);
+                    std::stringstream facess(line);
+                    TriangleIndex trig;
+                    facess >> tok;
+                    for (int ii = 0; ii < 3; ii++) {
+                        facess >> trig[ii] >> texID;
+                        trig[ii]--;
+                    }
+                    t.push_back(trig);
+                } else {
+                    TriangleIndex trig;
+                    for (int ii = 0; ii < 3; ii++) {
+                        ss >> trig[ii];
+                        trig[ii]--;
+                    }
+                    t.push_back(trig);
+                }
+            } else if (tok == texTok) {
+                Vector2f texcoord;
+                ss >> texcoord[0];
+                ss >> texcoord[1];
+            }
+        }
+        computeNormal();
+        f.close();
+
+        for (int triId = 0; triId < t.size(); ++triId) {
+            TriangleIndex& triIndex = t[triId];
+            triangles.push_back(new Triangle(v[triIndex[0]], v[triIndex[1]], v[triIndex[2]], material));
+        }
+
+        buildTree(rt, triangles, 0);
+    }
+
+    struct TriangleIndex {
+        TriangleIndex() {
+            x[0] = 0; x[1] = 0; x[2] = 0;
+        }
+        int &operator[](const int i) { return x[i]; }
+        // By Computer Graphics convention, counterclockwise winding is front face
+        int x[3]{};
+    };
+
+    std::vector<Vector3f> v; // point index pool
+    std::vector<TriangleIndex> t; // point indices for each triangle
+    std::vector<Vector3f> n; // normal vectors for each triangle
+    bool intersect(const Ray &r, Hit &h, float tmin) {
+        return queryIntersect(rt, r, h, tmin);
+    }
+
+    BoundPlane getBoundPlaneX() override {
+        if (rt != NULL) return rt->planeX;
+    }
+    BoundPlane getBoundPlaneY() override {
+        if (rt != NULL) return rt->planeY;
+    }
+    BoundPlane getBoundPlaneZ() override {
+        if (rt != NULL) return rt->planeZ;
+    }
+
+private:
+
+    // Normal can be used for light estimation
+    void computeNormal() {
+        n.resize(t.size());
+        for (int triId = 0; triId < (int) t.size(); ++triId) {
+            TriangleIndex& triIndex = t[triId];
+            Vector3f a = v[triIndex[1]] - v[triIndex[0]];
+            Vector3f b = v[triIndex[2]] - v[triIndex[0]];
+            b = Vector3f::cross(a, b);
+            n[triId] = b / b.length();
+        }
+    }
+    std::vector <Object3D*> triangles;
+};
+
+#endif
---
+++ RT/code/src/main.cpp
@@ -0,0 +1,86 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+
+#include "scene_parser.hpp"
+#include "image.hpp"
+#include "camera.hpp"
+#include "group.hpp"
+#include "light.hpp"
+
+#include <string>
+
+using namespace std;
+
+const int maxDep = 5;
+
+Vector3f rayTracing(const Ray &r, int dep, bool &isIntersect) {
+    Hit hit;
+    isIntersect = baseGroup->intersect(r, hit, 0);
+    if (!isIntersect)
+        return Vector3f(0);
+    if (dep > maxDep) {
+        Vector3f p = camRay.pointAtParameter(hit.getT());
+        Vector3f finalColor = Vector3f::ZERO;
+        for (int li = 0; li < nLights; ++li) {
+            Light *light = sceneParser.getLight(li);
+            Vector3f L, lightColor;
+            light->getIllumination(p, L, lightColor);
+            finalColor += hit.getMaterial()->Shade(camRay, hit, L, lightColor);
+        }
+        return finalColor;
+    }
+    
+}
+
+int main(int argc, char *argv[]) {
+    int timeStamp = clock();
+
+    for (int argNum = 1; argNum < argc; ++argNum) {
+        std::cout << "Argument " << argNum << " is: " << argv[argNum] << std::endl;
+    }
+
+    if (argc != 3) {
+        cout << "Usage: ./bin/PA1 <input scene file> <output bmp file>" << endl;
+        return 1;
+    }
+
+    SceneParser sceneParser(argv[1]);
+    Camera *camera = sceneParser.getCamera();
+    Group* baseGroup = sceneParser.getGroup();
+    Vector3f bgColor = sceneParser.getBackgroundColor();
+
+    int w = camera->getWidth(), h = camera->getHeight(),
+        nLights = sceneParser.getNumLights();
+    Image renderedImg(w, h);
+
+
+    for (int x = 0; x < w; ++x) {
+        for (int y = 0; y < h; ++y) {
+            Ray camRay = camera->generateRay(Vector2f(x, y));
+            Hit hit;
+            bool isIntersect = baseGroup->intersect(camRay, hit, 0);
+            if (isIntersect) {
+                Vector3f p = camRay.pointAtParameter(hit.getT());
+                Vector3f finalColor = Vector3f::ZERO;
+                for (int li = 0; li < nLights; ++li) {
+                    Light *light = sceneParser.getLight(li);
+                    Vector3f L, lightColor;
+                    light->getIllumination(p, L, lightColor);
+                    finalColor += hit.getMaterial()->Shade(camRay, hit, L, lightColor);
+                }
+                renderedImg.SetPixel(x, y, finalColor);
+            } else renderedImg.SetPixel(x, y, bgColor);
+        }
+    }
+    
+    renderedImg.SaveImage(argv[2]);
+    
+    cout << "Hello! Computer Graphics!" << endl;
+    cout << "time: " << (clock() - timeStamp) / 1e6 << " sec\n";
+    return 0;
+}
+
