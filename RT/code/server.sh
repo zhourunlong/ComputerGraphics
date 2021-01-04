6013f17cf95e85f306ec902e3fc64b8ece20447e
zhourunlong <zhourunlongvector@gmail.com>
2021-01-04 23:51:52 +0800

    prepare server environment

---
+++ RT/code/server.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+rm /etc/apt/sources.list
+cp sources.list /etc/apt/sources.list
+apt update
+apt upgrade
+apt install -y clang-9 libc++-9-dev libc++abi-9-dev cmake ninja-build
+apt install -y libz-dev libpng-dev libjpeg-dev libxrandr-dev libxinerama-dev libxcursor-dev
+apt install -y python3-dev python3-distutils python3-setuptools
+
+if [[ ! -d build ]]; then
+    mkdir -p build
+    cd build
+    cmake ..
+    cd ..
+fi
+
+cd build
+make -j
+cd ..
+
+mkdir -p output
+#bin/RT testcases/cornellbox.xml output/cornellbox.bmp
+#bin/RT testcases/smallpt.xml output/smallpt.bmp
+#bin/RT testcases/coffee/scene.xml output/coffee.bmp
+bin/RT testcases/kitchen/scene.xml output/kitchen.bmp
