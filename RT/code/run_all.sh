0b6821a67f7c560db4d4967e0443219f9458f607
zhourunlong <zhourunlongvector@gmail.com>
2021-01-09 22:09:24 +0800

    ?

--- RT/code/run_all.sh
+++ RT/code/run_all.sh
@@ -21,6 +21,6 @@ mkdir -p output
 #bin/RT testcases/coffee/scene.xml output/coffee.bmp
 #bin/RT testcases/kitchen/scene.xml output/kitchen.bmp
 #bin/RT testcases/spaceship/scene.xml output/spaceship.bmp
-#bin/RT testcases/cornellbox2.xml output/cornellbox2.bmp
+bin/RT testcases/cornellbox2.xml output/cornellbox2.bmp
 #bin/RT testcases/bathroom/scene.xml output/bathroom.bmp
-bin/RT testcases/spline.xml output/spline.bmp
+#bin/RT testcases/spline.xml output/spline.bmp
